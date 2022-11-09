9f2f92703dae482c76aeb8f2217c4e605b5c5c52
Rood <48691039+Babaloopey@users.noreply.github.com>
2022-11-09 18:11:40 +0100

    adjustment

--- Projekt Finished/main.js
+++ Projekt Finished/main.js
@@ -1,4 +1,73 @@
-import {ToDo, ToDoDto} from 'todo.js'
+class ToDoDto {
+  titel;
+  erledigt;
+
+  constructor(titel, erledigt) {
+    this.titel = titel;
+    this.erledigt = erledigt;
+  }
+}
+
+class ToDo extends EventTarget {
+  #titel = '';
+  #erledigt = '';
+
+  constructor(titel, erledigt) {
+    super();
+    this.#titel = titel;
+    this.#erledigt = erledigt;
+  }
+
+  get titel() {
+    return this.#titel;
+  }
+
+  set titel(titel) {
+    this.#titel = titel;
+  }
+
+  get erledigt() {
+    return this.#erledigt;
+  }
+
+  set erledigt(erledigt) {
+    this.#erledigt = erledigt;
+  }
+
+  element(index) {
+    const listElement = document.createElement('li');
+    const divElement = document.createElement('div');
+    const checkboxElement = document.createElement('input');
+    const spanElement = document.createElement('span');
+    const buttonElement = document.createElement('button');
+
+    listElement.appendChild(divElement);
+
+    divElement.appendChild(checkboxElement);
+    divElement.appendChild(spanElement);
+    divElement.appendChild(buttonElement);
+
+    checkboxElement.setAttribute('type', 'checkbox');
+    checkboxElement.setAttribute('id', `${index}`);
+
+    buttonElement.className = 'loeschen';
+
+    spanElement.innerText = this.#titel;
+    buttonElement.innerText = 'Löschen';
+
+    if (this.#erledigt) {
+      checkboxElement.setAttribute('checked', 'checked');
+      divElement.className = 'erledigt';
+    }
+
+
+    buttonElement.addEventListener('click', () => {
+      this.dispatchEvent(new Event('loeschen'));
+    });
+
+    return listElement;
+  }
+}
 
 
 let todos = [];
